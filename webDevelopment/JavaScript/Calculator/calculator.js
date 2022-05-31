b02913c495b07d1aead99090066239f1f29e80e5
ATOIsus <sabinmaharjan884@gmail.com>
2022-05-31 11:24:04 +0545

    JQuery and Singleton pattern done.

---
+++ webDevelopment/JavaScript/Calculator/calculator.js
@@ -0,0 +1,11 @@
+
+function takeNum(val){
+    let inputElem1 = document.getElementById("val1");
+    // let inputElem2 = document.getElementById("val2");
+
+    // let c = parseInt(inputElem1.value) + parseInt(inputElem2.value);
+
+    console.log(val)
+    document.getElementsByClassName('screen').value = val;
+
+}
