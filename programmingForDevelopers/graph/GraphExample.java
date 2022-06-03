0f7309807ef2f06889b5a40ff2ae76ac6092f008
ATOIsus <sabinmaharjan884@gmail.com>
2022-06-03 08:52:33 +0545

    Graph implemented.

--- programmingForDevelopers/graph/GraphExample.java
+++ programmingForDevelopers/graph/GraphExample.java
@@ -1,4 +1,55 @@
 package programmingForDevelopers.graph;
 
 public class GraphExample {
+    int vertices;
+    int matrix[][];
+
+    GraphExample(int vertices) {
+        this.vertices = vertices;
+        matrix = new int[vertices][vertices];
+    }
+
+    public void addEdge(int source, int destination) {
+        matrix[source][destination] = 1;
+        matrix[destination][source] = 1;
+    }
+
+    public void printGraph() {
+        //travelling to rows
+        for (int i = 0; i < vertices; i++) {
+            //travelling to columns
+            for (int j = 0; j < vertices; j++) {
+                //printing values of matrix
+                System.out.print(matrix[i][j] + "  ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public void printAdjEdges() {
+        //travelling to rows
+        for (int i = 0; i < vertices; i++) {
+            System.out.print(i + " is connected to: ");
+            for (int j = 0; j < vertices; j++) {
+                if (matrix[i][j] != 0) {
+                    System.out.print(j + ", ");
+                }
+
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphExample g = new GraphExample(5);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.printGraph();
+        g.printAdjEdges();
+    }
+
 }
