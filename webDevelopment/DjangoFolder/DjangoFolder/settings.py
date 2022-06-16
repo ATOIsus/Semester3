4819246a993558280be73d88a301e27bee97e579
ATOIsus <sabinmaharjan884@gmail.com>
2022-06-16 09:18:49 +0545

    Variable passed from View to Template and XAMPP started.

--- webDevelopment/DjangoFolder/DjangoFolder/settings.py
+++ webDevelopment/DjangoFolder/DjangoFolder/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DjangoFolder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'game',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT' : '3307' 
     }
 }
 
--- webDevelopment/DjangoFolder/app/views.py
+++ webDevelopment/DjangoFolder/app/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    print(request)
+    data = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+    }
+    return render(request, 'index.html', {'datas': data})
 
 
 def about(request):
