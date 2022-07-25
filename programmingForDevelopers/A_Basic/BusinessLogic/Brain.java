9d8f5a9022a5ae19672490f221b93f888462333f
ATOIsus <sabinmaharjan884@gmail.com>
2022-07-25 14:45:45 +0545

    Basic skeleton for GUI application.

---
+++ programmingForDevelopers/A_Basic/BusinessLogic/Brain.java
@@ -0,0 +1,29 @@
+package programmingForDevelopers.A_Basic.BusinessLogic;
+
+public class Brain {
+
+    private int firstNum;
+    private int secondNum;
+
+    public Brain(int firstNum, int secondNum) {
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+
+    }
+
+
+    public int add() {
+        return firstNum + secondNum;
+    }
+
+
+    public int firstNumber() {
+        return firstNum;
+    }
+
+
+    public int secondNumber() {
+        return secondNum;
+    }
+
+}
